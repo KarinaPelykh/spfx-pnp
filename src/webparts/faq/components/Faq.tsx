28d0ff4745f3b8aed19876b5218b1c16a95f76d2
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2025-07-04 22:32:26 +0200

    feat: FAQ list,with  sharepoint and  pnp

---
+++ src/webparts/faq/components/Faq.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { useEffect, useState, type ReactNode } from "react";
+import type { IFaqProps } from "./IFaqProps";
+import { SPFI } from "@pnp/sp";
+import { IFAQ } from "../../../interfaces";
+import { getSP } from "../../../pnpjsConfig";
+import { Accordion } from "@pnp/spfx-controls-react/lib/Accordion";
+
+const Fag = (props: IFaqProps): ReactNode => {
+  // const LOG_SOURCE = "Faq Webpart";
+
+  const LIST_NAME = "FAQ";
+
+  const _sp: SPFI = getSP(props.context);
+
+  const [faqList, setFaqList] = useState<IFAQ[]>([]);
+
+  const getFaq = async (): Promise<void> => {
+    const items = _sp.web.lists
+      .getByTitle(LIST_NAME)
+      .items.select()
+      .orderBy("Letter", true)
+      .orderBy("Title", true)();
+
+    setFaqList(
+      (await items).map((item: any) => {
+        return {
+          Id: item.Id,
+          Title: item.Title,
+          Body: item.Body,
+          Letter: item.Letter,
+        };
+      })
+    );
+  };
+
+  useEffect(() => {
+    getFaq()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <>
+      {faqList.map((o: IFAQ, index: number) => {
+        return (
+          <Accordion title={o.Title} key={index} defaultCollapsed={true}>
+            {o.Body}
+          </Accordion>
+        );
+      })}
+    </>
+  );
+};
+export default Fag;
